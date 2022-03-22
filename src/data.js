87ee367f104a7c74bc5d36b1f20d55ff60e2a57e
Cuper99 <binzariion@gmail.com>
2022-03-22 15:22:23 +0200

    added project

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data  = {
+    products:[
+        {
+            id: '1',
+            name :'Legume',
+            price:50,
+            Image:'src\\img\thumb-350-417957.jpg',
+        },
+        {
+            id: '2',
+            name :'Fructe',
+            price:100,
+            Image:'src\\img\fructe.jpeg',
+        },
+        {
+            id: '3',
+            name :'Fasole',
+            price:1200,
+            Image:'src\\img\boabe-fasole.jpg',
+        },
+    ],
+};
+export default data;
